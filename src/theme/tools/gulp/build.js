ac159666317f0cda284adf6ca9f632d8070416ad
Bassem <b.mamar@idscan.com>
2018-01-18 18:43:19 +0300

    enhance theme files build

---
+++ src/theme/tools/gulp/build.js
@@ -0,0 +1,10 @@
+var gutil = require('gulp-util');
+
+// merge with default parameters
+var args = Object.assign({'prod': false, default: true, angular: false}, gutil.env);
+
+var configs = {angular: './../conf/angular.json'};
+var config = configs.default;
+// angular flag true or path name has angular
+  config = configs.angular;
+module.exports = require(config);
