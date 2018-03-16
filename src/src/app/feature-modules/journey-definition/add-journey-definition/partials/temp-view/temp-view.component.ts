9db4a9d00967f8eafce10362744d1ef9c72c7297
Safwat Ghannoum <s.ghannoum@idscan.com>
2018-03-16 10:54:30 +0300

    add selfie tag

--- src/src/app/feature-modules/journey-definition/add-journey-definition/partials/temp-view/temp-view.component.ts
+++ src/src/app/feature-modules/journey-definition/add-journey-definition/partials/temp-view/temp-view.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: '',
+  selector: 'app-temp-view',
   templateUrl: './temp-view.component.html',
   styleUrls: ['./temp-view.component.scss']
 })
